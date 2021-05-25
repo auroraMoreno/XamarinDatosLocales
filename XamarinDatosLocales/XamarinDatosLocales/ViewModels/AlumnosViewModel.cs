1180182601487960645199fcd66535401823743e
Aurora Moreno Lopez <aurora.moreno.lopez@tajamar365.com>
2021-05-25 14:23:50 +0200

    commit

---
+++ XamarinDatosLocales/XamarinDatosLocales/ViewModels/AlumnosViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+using XamarinDatosLocales.Models;
+using XamarinDatosLocales.Repository;
+
+namespace XamarinDatosLocales.ViewModels
+{
+    public class AlumnosViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public RepositoryAlumnos repo;
+        public void RaisePropertyChanged(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private String _Dato;
+        public String Datos
+        {
+            get { return this._Dato; }
+            set
+            {
+                this._Dato = value;
+                RaisePropertyChanged("Dato");
+            }
+        }
+
+        private List<Alumno> _Alumnos;
+        public List<Alumno> Alumnos
+        {
+            get { return this._Alumnos; }
+            set
+            {
+                this._Alumnos = value;
+                RaisePropertyChanged("Alumnos");
+            }
+        }
+
+        private List<Alumno> MostrarAlumnos()
+        {
+            List<Alumno> alumnos = this.repo.GetAlumnos();
+            return alumnos;
+        }
+
+        public Command MostrarDatos
+        {
+            get
+            {
+                return new Command(() =>
+                {
+                    this.Alumnos = MostrarAlumnos();
+                });
+            }
+        }
+    }
+}
